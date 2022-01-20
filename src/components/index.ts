6fcfd0843d3a66be19c047d8b672daec13d9e4d7
Erez Bracha <erezushi900@gmail.com>
2022-01-20 23:18:02 +0200

    Added Index file ro components folder

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './AppHeader';
+export * from './DetailsModal';
+export * from './ExportModal';
+export * from './LoadingSnackbar';
+export * from './OptionsBox';
+export * from './PokemonCard';
+export * from './PokemonList';

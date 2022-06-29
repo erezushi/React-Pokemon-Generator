2940e93081790305ac182191cd667d35407e138c
Erez Bracha <erezushi900@gmail.com>
2022-06-29 23:06:11 +0300

    Reformatted type definitions

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -7,20 +7,22 @@ export interface IPokemonInstance {
   fullName: string
 }
 
-type abilities = {
-  ability: {
+type pokeApiGeneric = {
     name: string,
     url: string
-  },
-  'is_hidden': boolean,
+};
+
+type abilities = {
+  ability: pokeApiGeneric,
+  is_hidden: boolean,
   slot: number,
-}[]
+}[];
 
 type stats = {
-  'base_stat': number,
+  base_stat: number,
   effort: number,
-  stat: any,
-}[]
+  stat: pokeApiGeneric
+}[];
 
 export interface IPokemonDetails {
   abilities: abilities,
@@ -30,18 +32,18 @@ export interface IPokemonDetails {
 
 export interface IPokemonResponse {
   abilities: abilities,
-  'base_experience': number,
+  base_experience: number,
   forms: any[],
-  'game_indices': any[],
+  game_indices: any[],
   height: number,
-  'held_items': any[],
+  held_items: any[],
   id: number,
-  'is_default': boolean,
-  'location_area_encounters': string,
+  is_default: boolean,
+  location_area_encounters: string,
   moves: any[],
   name: string,
   order: number,
-  'past_types': any[],
+  past_types: any[],
   species: any,
   sprites: any,
   stats: stats,
@@ -50,36 +52,36 @@ export interface IPokemonResponse {
 }
 
 export interface IPokemonSpeciesResponse {
-  'base_happiness': number,
-  'capture_rate': number,
+  base_happiness: number,
+  capture_rate: number,
   color: any,
-  'egg_groups': any[],
-  'evolution_chain': any,
-  'evolves_from_species': any | null,
-  'flavor_text_entries': any[],
-  'form_descriptions': any[],
-  'forms_switchable': boolean,
-  'gender_rate': -1|0|1|2|4|6|7|8,
+  egg_groups: any[],
+  evolution_chain: any,
+  evolves_from_species: any | null,
+  flavor_text_entries: any[],
+  form_descriptions: any[],
+  forms_switchable: boolean,
+  gender_rate: -1|0|1|2|4|6|7|8,
   genera: any[],
   generation: any,
-  'growth_rate': any[],
-  'habitat': any | null,
-  'has_gender_differences': boolean,
-  'hatch_counter': number,
+  growth_rate: any[],
+  habitat: any | null,
+  has_gender_differences: boolean,
+  hatch_counter: number,
   id: number,
-  'is_baby': boolean,
-  'is_legendary': boolean,
-  'is_mythical': boolean,
+  is_baby: boolean,
+  is_legendary: boolean,
+  is_mythical: boolean,
   name: string,
   names: any[],
   order: number,
-  'pal_park_encounters': any[],
-  'pokedex_numbers': any[],
+  pal_park_encounters: any[],
+  pokedex_numbers: any[],
   shape: any,
   varieties: any[]
 }
 
-export type checkBoxState = 'checked' | 'indeterminate' | 'none'
+export type checkBoxState = 'checked' | 'indeterminate' | 'none';
 
 export interface ISettings {
   unique: boolean,
